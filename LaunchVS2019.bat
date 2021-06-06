31e402934e3da10cd8a9d4c355d33158cb8f5b6a
JasperDre <jasperdre@gmail.com>
2021-06-06 14:10:38 +0200

    Add initial project with supporting files for setup

---
+++ LaunchVS2019.bat
@@ -0,0 +1,1 @@
+start Generated/LearnOpenGL.sln
---
+++ Source/LearnOpenGL.cpp
@@ -0,0 +1,71 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+
+void ErrorCallback(int anError, const char* aDescription)
+{
+    printf("%i %s\n", anError, aDescription);
+}
+
+void FrameBufferSizeCallback(GLFWwindow* /*aWindow*/, int aWidth, int aHeight)
+{
+    glViewport(0, 0, aWidth, aHeight);
+}
+
+void ProcessInput(GLFWwindow* aWindow)
+{
+    if (glfwGetKey(aWindow, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(aWindow, true);
+}
+
+int main()
+{
+    glfwSetErrorCallback(ErrorCallback);
+
+    if (!glfwInit())
+    {
+        std::cout << "Failed to initialize GLFW" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", nullptr, nullptr);
+    if (!window)
+    {
+        std::cout << "Failed to create a GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glViewport(0, 0, 800, 600);
+
+    glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
+
+    while (!glfwWindowShouldClose(window))
+    {
+        ProcessInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+
+    return 0;
+}
