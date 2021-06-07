66951599c068d320dc3529ea35f6a17e87000d65
JasperDre <jasperdre@gmail.com>
2021-06-07 14:25:38 +0200

    Add cube rendering although they are triangles

--- Source/LearnOpenGL.cpp
+++ Source/LearnOpenGL.cpp
@@ -101,6 +101,7 @@ int main()
         return -1;
     }
 
+	glEnable(GL_DEPTH_TEST);
     glViewport(0, 0, 800, 600);
 
     glfwSetFramebufferSizeCallback(window, FrameBufferSizeCallback);
@@ -159,28 +160,50 @@ int main()
 
 	CheckGLError(window);
 
-	glm::mat4 matrix = glm::mat4(1.0f);
+	glm::mat4 view = glm::mat4(1.0f);
+	view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+	glm::mat4 projection = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 0.1f, 100.0f);
+
+	glm::vec3 cubePositions[] = {
+	glm::vec3(0.0f,  0.0f,  0.0f),
+	glm::vec3(2.0f,  5.0f, -15.0f),
+	glm::vec3(-1.5f, -2.2f, -2.5f),
+	glm::vec3(-3.8f, -2.0f, -12.3f),
+	glm::vec3(2.4f, -0.4f, -3.5f),
+	glm::vec3(-1.7f,  3.0f, -7.5f),
+	glm::vec3(1.3f, -2.0f, -2.5f),
+	glm::vec3(1.5f,  2.0f, -2.5f),
+	glm::vec3(1.5f,  0.2f, -1.5f),
+	glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
 
     while (!glfwWindowShouldClose(window))
     {
         ProcessInput(window);
 
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 		
 		glActiveTexture(GL_TEXTURE0);
 		glBindTexture(GL_TEXTURE_2D, containerTexture.GetID());
 		glActiveTexture(GL_TEXTURE1);
 		glBindTexture(GL_TEXTURE_2D, awesomeTexture.GetID());
 
-		glm::vec3 translation = glm::vec3(0.00001f, -0.00001f, 0.0f);
-		matrix = glm::translate(matrix, translation);
-
 		shaderProgram.Use();
-		shaderProgram.SetMatrix4x4("transform", matrix);
+		shaderProgram.SetMatrix4x4("view", view);
+		shaderProgram.SetMatrix4x4("projection", projection);
 
 		glBindVertexArray(VAO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		for (int index = 0; index < 10; index++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[index]);
+			float angle = 20.0f * index;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			shaderProgram.SetMatrix4x4("model", model);
+
+			glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		}
 
         glfwSwapBuffers(window);
         glfwPollEvents();
